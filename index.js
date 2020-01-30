4b20fba35897e9526eaee2d0b5b99c734dea79f8
aymankhawaldeh <57315054+aymankhawaldeh@users.noreply.github.com>
2020-01-30 18:57:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+'use strict';
+
+const mongoose = require('mongoose');
+const server = require('./collection/server.js');
+
+const MONOGDB_URI = 'mongodb://localhost:27017/lab8db';
+
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+mongoose.connect(MONOGDB_URI, mongooseOptions);
+
+server.start();
